25d60a92e2423cf29d61701135010b46a7472d1c
Jemar <j.medina0126@gmail.com>
2023-12-18 10:56:30 +0000

    Update GitHub and LinkedIn links in Footer component

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -71,7 +71,7 @@ const Footer = (props) => {
           </a>
         )}
         {gitHub && (
-          <a href={`https://github.com/${gitHub}`} target="_blank" rel="noopener noreferrer">
+          <a href={`https://github.com/jmedina0126/`} target="_blank" rel="noopener noreferrer">
             <img src={gitHubIcon} alt="GitHub" className="socialIcon" />
           </a>
         )}
@@ -86,7 +86,7 @@ const Footer = (props) => {
         )}
         {linkedIn && (
           <a
-            href={`https://www.linkedin.com/in/${linkedIn}`}
+            href={`https://www.linkedin.com/in/jemar-medina/`}
             target="_blank"
             rel="noopener noreferrer"
           >
