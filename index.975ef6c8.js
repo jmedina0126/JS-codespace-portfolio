7687a2b33a495a882883fbbc276b46089d4bc111
Jemar <j.medina0126@gmail.com>
2023-12-22 11:15:53 +0000

    Updates

--- index.975ef6c8.js
+++ index.975ef6c8.js
@@ -27585,7 +27585,7 @@ var _youtubeSvgDefault = parcelHelpers.interopDefault(_youtubeSvg);
                         columnNumber: 11
                     }, undefined),
                     gitHub && /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("a", {
-                        href: `https://github.com/${gitHub}`,
+                        href: `https://github.com/jmedina0126/`,
                         target: "_blank",
                         rel: "noopener noreferrer",
                         children: /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("img", {
@@ -27621,7 +27621,7 @@ var _youtubeSvgDefault = parcelHelpers.interopDefault(_youtubeSvg);
                         columnNumber: 11
                     }, undefined),
                     linkedIn && /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("a", {
-                        href: `https://www.linkedin.com/in/${linkedIn}`,
+                        href: `https://www.linkedin.com/in/jemar-medina/`,
                         target: "_blank",
                         rel: "noopener noreferrer",
                         children: /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("img", {
@@ -28615,7 +28615,7 @@ var _propTypesDefault = parcelHelpers.interopDefault(_propTypes);
  * Home background image
  *
  * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
+ * directory and import here for use. Then, set imageAltText to string that
  * represents what you see in that image.
  *
  *
@@ -28670,7 +28670,8 @@ const Home = ({ name , title  })=>{
                 style: {
                     position: "absolute",
                     bottom: "3rem",
-                    left: "50%"
+                    left: "50%",
+                    animation: "bounce 2s infinite"
                 },
                 children: /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("img", {
                     src: (0, _downArrowSvgDefault.default),
@@ -28681,7 +28682,7 @@ const Home = ({ name , title  })=>{
                     alt: imageAltText
                 }, void 0, false, {
                     fileName: "src/Components/Home.jsx",
-                    lineNumber: 36,
+                    lineNumber: 43,
                     columnNumber: 9
                 }, undefined)
             }, void 0, false, {
@@ -28740,6 +28741,7 @@ var _jsxDevRuntime = require("react/jsx-dev-runtime");
  * as you continue to learn and create.
  */ var _react = require("react");
 var _reactDefault = parcelHelpers.interopDefault(_react);
+var _reactIntersectionObserver = require("react-intersection-observer");
 /**
  * Desk image
  *
@@ -28750,6 +28752,7 @@ var _reactDefault = parcelHelpers.interopDefault(_react);
  * freely use on your site.
  */ var _designDeskJpeg = require("../images/design-desk.jpeg");
 var _designDeskJpegDefault = parcelHelpers.interopDefault(_designDeskJpeg);
+var _s = $RefreshSig$();
 const imageAltText = "desktop with books and laptop";
 /**
  * Project list
@@ -28779,6 +28782,10 @@ const imageAltText = "desktop with books and laptop";
     }, 
 ];
 const Portfolio = ()=>{
+    _s();
+    const { ref , inView  } = (0, _reactIntersectionObserver.useInView)({
+        triggerOnce: true
+    });
     return /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("section", {
         className: "padding",
         id: "portfolio",
@@ -28790,7 +28797,7 @@ const Portfolio = ()=>{
                 children: "Portfolio"
             }, void 0, false, {
                 fileName: "src/Components/Portfolio.jsx",
-                lineNumber: 62,
+                lineNumber: 65,
                 columnNumber: 7
             }, undefined),
             /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("div", {
@@ -28806,21 +28813,25 @@ const Portfolio = ()=>{
                             alignSelf: "center"
                         },
                         children: /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("img", {
+                            ref: ref,
                             src: (0, _designDeskJpegDefault.default),
                             style: {
                                 height: "90%",
                                 width: "100%",
-                                objectFit: "cover"
+                                objectFit: "cover",
+                                opacity: inView ? 1 : 0,
+                                transition: "opacity 1s",
+                                animation: inView ? "500ms ease-out 0s 1 slideIn" : ""
                             },
                             alt: imageAltText
                         }, void 0, false, {
                             fileName: "src/Components/Portfolio.jsx",
-                            lineNumber: 65,
+                            lineNumber: 68,
                             columnNumber: 11
                         }, undefined)
                     }, void 0, false, {
                         fileName: "src/Components/Portfolio.jsx",
-                        lineNumber: 64,
+                        lineNumber: 67,
                         columnNumber: 9
                     }, undefined),
                     /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("div", {
@@ -28839,12 +28850,12 @@ const Portfolio = ()=>{
                                             children: project.title
                                         }, void 0, false, {
                                             fileName: "src/Components/Portfolio.jsx",
-                                            lineNumber: 75,
+                                            lineNumber: 86,
                                             columnNumber: 17
                                         }, undefined)
                                     }, void 0, false, {
                                         fileName: "src/Components/Portfolio.jsx",
-                                        lineNumber: 74,
+                                        lineNumber: 85,
                                         columnNumber: 15
                                     }, undefined),
                                     /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("p", {
@@ -28852,33 +28863,38 @@ const Portfolio = ()=>{
                                         children: project.description
                                     }, void 0, false, {
                                         fileName: "src/Components/Portfolio.jsx",
-                                        lineNumber: 77,
+                                        lineNumber: 88,
                                         columnNumber: 15
                                     }, undefined)
                                 ]
                             }, project.title, true, {
                                 fileName: "src/Components/Portfolio.jsx",
-                                lineNumber: 73,
+                                lineNumber: 84,
                                 columnNumber: 13
                             }, undefined))
                     }, void 0, false, {
                         fileName: "src/Components/Portfolio.jsx",
-                        lineNumber: 71,
+                        lineNumber: 82,
                         columnNumber: 9
                     }, undefined)
                 ]
             }, void 0, true, {
                 fileName: "src/Components/Portfolio.jsx",
-                lineNumber: 63,
+                lineNumber: 66,
                 columnNumber: 7
             }, undefined)
         ]
     }, void 0, true, {
         fileName: "src/Components/Portfolio.jsx",
-        lineNumber: 61,
+        lineNumber: 64,
         columnNumber: 5
     }, undefined);
 };
+_s(Portfolio, "oyd/E8SD7Fx4uOp6P7gVV2pVlaE=", false, function() {
+    return [
+        (0, _reactIntersectionObserver.useInView)
+    ];
+});
 _c = Portfolio;
 exports.default = Portfolio;
 var _c;
@@ -28889,9 +28905,260 @@ $RefreshReg$(_c, "Portfolio");
   window.$RefreshReg$ = prevRefreshReg;
   window.$RefreshSig$ = prevRefreshSig;
 }
-},{"react/jsx-dev-runtime":"iTorj","react":"21dqq","../images/design-desk.jpeg":"jSSP3","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3","@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js":"km3Ru"}],"jSSP3":[function(require,module,exports) {
+},{"react/jsx-dev-runtime":"iTorj","react":"21dqq","react-intersection-observer":"hOIJm","../images/design-desk.jpeg":"jSSP3","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3","@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js":"km3Ru"}],"hOIJm":[function(require,module,exports) {
+var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
+parcelHelpers.defineInteropFlag(exports);
+parcelHelpers.export(exports, "InView", ()=>InView);
+parcelHelpers.export(exports, "defaultFallbackInView", ()=>defaultFallbackInView);
+parcelHelpers.export(exports, "observe", ()=>observe);
+parcelHelpers.export(exports, "useInView", ()=>useInView);
+// src/InView.tsx
+var _react = require("react");
+"use client";
+var __defProp = Object.defineProperty;
+var __defNormalProp = (obj, key, value)=>key in obj ? __defProp(obj, key, {
+        enumerable: true,
+        configurable: true,
+        writable: true,
+        value
+    }) : obj[key] = value;
+var __publicField = (obj, key, value)=>{
+    __defNormalProp(obj, typeof key !== "symbol" ? key + "" : key, value);
+    return value;
+};
+// src/observe.ts
+var observerMap = /* @__PURE__ */ new Map();
+var RootIds = /* @__PURE__ */ new WeakMap();
+var rootId = 0;
+var unsupportedValue = void 0;
+function defaultFallbackInView(inView) {
+    unsupportedValue = inView;
+}
+function getRootId(root) {
+    if (!root) return "0";
+    if (RootIds.has(root)) return RootIds.get(root);
+    rootId += 1;
+    RootIds.set(root, rootId.toString());
+    return RootIds.get(root);
+}
+function optionsToId(options) {
+    return Object.keys(options).sort().filter((key)=>options[key] !== void 0).map((key)=>{
+        return `${key}_${key === "root" ? getRootId(options.root) : options[key]}`;
+    }).toString();
+}
+function createObserver(options) {
+    let id = optionsToId(options);
+    let instance = observerMap.get(id);
+    if (!instance) {
+        const elements = /* @__PURE__ */ new Map();
+        let thresholds;
+        const observer = new IntersectionObserver((entries)=>{
+            entries.forEach((entry)=>{
+                var _a;
+                const inView = entry.isIntersecting && thresholds.some((threshold)=>entry.intersectionRatio >= threshold);
+                if (options.trackVisibility && typeof entry.isVisible === "undefined") entry.isVisible = inView;
+                (_a = elements.get(entry.target)) == null || _a.forEach((callback)=>{
+                    callback(inView, entry);
+                });
+            });
+        }, options);
+        thresholds = observer.thresholds || (Array.isArray(options.threshold) ? options.threshold : [
+            options.threshold || 0
+        ]);
+        instance = {
+            id,
+            observer,
+            elements
+        };
+        observerMap.set(id, instance);
+    }
+    return instance;
+}
+function observe(element, callback, options = {}, fallbackInView = unsupportedValue) {
+    if (typeof window.IntersectionObserver === "undefined" && fallbackInView !== void 0) {
+        const bounds = element.getBoundingClientRect();
+        callback(fallbackInView, {
+            isIntersecting: fallbackInView,
+            target: element,
+            intersectionRatio: typeof options.threshold === "number" ? options.threshold : 0,
+            time: 0,
+            boundingClientRect: bounds,
+            intersectionRect: bounds,
+            rootBounds: bounds
+        });
+        return ()=>{};
+    }
+    const { id , observer , elements  } = createObserver(options);
+    let callbacks = elements.get(element) || [];
+    if (!elements.has(element)) elements.set(element, callbacks);
+    callbacks.push(callback);
+    observer.observe(element);
+    return function unobserve() {
+        callbacks.splice(callbacks.indexOf(callback), 1);
+        if (callbacks.length === 0) {
+            elements.delete(element);
+            observer.unobserve(element);
+        }
+        if (elements.size === 0) {
+            observer.disconnect();
+            observerMap.delete(id);
+        }
+    };
+}
+// src/InView.tsx
+function isPlainChildren(props) {
+    return typeof props.children !== "function";
+}
+var InView = class extends _react.Component {
+    constructor(props){
+        super(props);
+        __publicField(this, "node", null);
+        __publicField(this, "_unobserveCb", null);
+        __publicField(this, "handleNode", (node)=>{
+            if (this.node) {
+                this.unobserve();
+                if (!node && !this.props.triggerOnce && !this.props.skip) this.setState({
+                    inView: !!this.props.initialInView,
+                    entry: void 0
+                });
+            }
+            this.node = node ? node : null;
+            this.observeNode();
+        });
+        __publicField(this, "handleChange", (inView, entry)=>{
+            if (inView && this.props.triggerOnce) this.unobserve();
+            if (!isPlainChildren(this.props)) this.setState({
+                inView,
+                entry
+            });
+            if (this.props.onChange) this.props.onChange(inView, entry);
+        });
+        this.state = {
+            inView: !!props.initialInView,
+            entry: void 0
+        };
+    }
+    componentDidMount() {
+        this.unobserve();
+        this.observeNode();
+    }
+    componentDidUpdate(prevProps) {
+        if (prevProps.rootMargin !== this.props.rootMargin || prevProps.root !== this.props.root || prevProps.threshold !== this.props.threshold || prevProps.skip !== this.props.skip || prevProps.trackVisibility !== this.props.trackVisibility || prevProps.delay !== this.props.delay) {
+            this.unobserve();
+            this.observeNode();
+        }
+    }
+    componentWillUnmount() {
+        this.unobserve();
+    }
+    observeNode() {
+        if (!this.node || this.props.skip) return;
+        const { threshold , root , rootMargin , trackVisibility , delay , fallbackInView  } = this.props;
+        this._unobserveCb = observe(this.node, this.handleChange, {
+            threshold,
+            root,
+            rootMargin,
+            // @ts-ignore
+            trackVisibility,
+            // @ts-ignore
+            delay
+        }, fallbackInView);
+    }
+    unobserve() {
+        if (this._unobserveCb) {
+            this._unobserveCb();
+            this._unobserveCb = null;
+        }
+    }
+    render() {
+        const { children  } = this.props;
+        if (typeof children === "function") {
+            const { inView , entry  } = this.state;
+            return children({
+                inView,
+                entry,
+                ref: this.handleNode
+            });
+        }
+        const { as , triggerOnce , threshold , root , rootMargin , onChange , skip , trackVisibility , delay , initialInView , fallbackInView , ...props } = this.props;
+        return _react.createElement(as || "div", {
+            ref: this.handleNode,
+            ...props
+        }, children);
+    }
+};
+function useInView({ threshold , delay , trackVisibility , rootMargin , root , triggerOnce , skip , initialInView , fallbackInView , onChange  } = {}) {
+    var _a;
+    const [ref, setRef] = _react.useState(null);
+    const callback = _react.useRef();
+    const [state, setState] = _react.useState({
+        inView: !!initialInView,
+        entry: void 0
+    });
+    callback.current = onChange;
+    _react.useEffect(()=>{
+        if (skip || !ref) return;
+        let unobserve;
+        unobserve = observe(ref, (inView, entry)=>{
+            setState({
+                inView,
+                entry
+            });
+            if (callback.current) callback.current(inView, entry);
+            if (entry.isIntersecting && triggerOnce && unobserve) {
+                unobserve();
+                unobserve = void 0;
+            }
+        }, {
+            root,
+            rootMargin,
+            threshold,
+            // @ts-ignore
+            trackVisibility,
+            // @ts-ignore
+            delay
+        }, fallbackInView);
+        return ()=>{
+            if (unobserve) unobserve();
+        };
+    }, // We break the rule here, because we aren't including the actual `threshold` variable
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [
+        // If the threshold is an array, convert it to a string, so it won't change between renders.
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        Array.isArray(threshold) ? threshold.toString() : threshold,
+        ref,
+        root,
+        rootMargin,
+        triggerOnce,
+        skip,
+        trackVisibility,
+        fallbackInView,
+        delay
+    ]);
+    const entryTarget = (_a = state.entry) == null ? void 0 : _a.target;
+    const previousEntryTarget = _react.useRef();
+    if (!ref && entryTarget && !triggerOnce && !skip && previousEntryTarget.current !== entryTarget) {
+        previousEntryTarget.current = entryTarget;
+        setState({
+            inView: !!initialInView,
+            entry: void 0
+        });
+    }
+    const result = [
+        setRef,
+        state.inView,
+        state.entry
+    ];
+    result.ref = result[0];
+    result.inView = result[1];
+    result.entry = result[2];
+    return result;
+}
+
+},{"react":"21dqq","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"jSSP3":[function(require,module,exports) {
 module.exports = require("./helpers/bundle-url").getBundleURL("bLxZJ") + "design-desk.df4cd0cf.jpeg" + "?" + Date.now();
 
-},{"./helpers/bundle-url":"lgJ39"}],"lW6qc":[function() {},{}]},["1xC6H","ShInH","8lqZg"], "8lqZg", "parcelRequire6158")
+},{"./helpers/bundle-url":"lgJ39"}],"lW6qc":[function() {},{}]},["1xC6H","ShInH","8lqZg"], "8lqZg", "parcelRequire20be")
 
 //# sourceMappingURL=index.975ef6c8.js.map
